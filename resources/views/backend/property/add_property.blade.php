3452cb37187371a176470f20853f572574283c08
adelsabagh <adolsabagh1996@gmail.com>
2023-10-22 12:17:19 +0200

    Show Image Using Javascript

--- resources/views/backend/property/add_property.blade.php
+++ resources/views/backend/property/add_property.blade.php
@@ -46,14 +46,16 @@
                                     <div class="col-sm-6">
                                         <div class="mb-3">
                                             <label class="form-label">Main Thumbnail</label>
-                                            <input type="file" name="max_price" class="form-control">
+                                            <input type="file" name="property_thumbnail" class="form-control" onchange="mainThumUrl(this)">
+                                            <img src="" id="mainThumb">
                                         </div>
                                     </div><!-- Col -->
 
                                     <div class="col-sm-6">
                                         <div class="mb-3">
                                             <label class="form-label">Multiple Image</label>
-                                            <input type="file" name="max_price" class="form-control">
+                                            <input type="file" name="multi_img[]" class="form-control" id="multiImg" multiple="">
+                                            <div class="row" id="preview_img"> </div>
                                         </div>
                                     </div><!-- Col -->
 
@@ -109,6 +111,7 @@
 
 
     <script type="text/javascript">
+
         $(document).ready(function (){
             $('#myForm').validate({
                 rules: {
@@ -135,6 +138,48 @@
                 unhighlight : function(element, errorClass, validClass){
                     $(element).removeClass('is-invalid');
                 },
+            });
+        });
+
+    </script>
+
+    <script type="text/javascript">
+        function mainThumUrl(input) {
+            if(input.files && input.files[0]) {
+                var reader = new FileReader();
+                reader.onload = function (e) {
+                    $('#mainThumb').attr('src', e.target.result).width(80).height(80);
+                };
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
+    </script>
+
+    <script>
+
+        $(document).ready(function(){
+            $('#multiImg').on('change', function(){ //on file input change
+                if (window.File && window.FileReader && window.FileList && window.Blob) //check File API supported browser
+                {
+                    var data = $(this)[0].files; //this file data
+
+                    $.each(data, function(index, file){ //loop though each file
+                        if(/(\.|\/)(gif|jpe?g|png|webp)$/i.test(file.type)){ //check supported file type
+                            var fRead = new FileReader(); //new filereader
+                            fRead.onload = (function(file){ //trigger function on successful read
+                                return function(e) {
+                                    var img = $('<img/>').addClass('thumb').attr('src', e.target.result) .width(100)
+                                        .height(80); //create image element
+                                    $('#preview_img').append(img); //append image to output element
+                                };
+                            })(file);
+                            fRead.readAsDataURL(file); //URL representing the file's data.
+                        }
+                    });
+
+                }else{
+                    alert("Your browser doesn't support File API!"); //if File API is absent
+                }
             });
         });
 
