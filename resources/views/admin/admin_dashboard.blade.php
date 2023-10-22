9414cf9466d6df52f6c65e3cff417e9e25bee82d
adelsabagh <adolsabagh1996@gmail.com>
2023-10-22 14:29:26 +0200

    Add Property Database and Page Design Part 3

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -139,5 +139,10 @@
 <script src="{{ asset('backend/assets/js/tags-input.js') }}"></script>
 <!-- End Multiple select -->
 
+<!-- Start tinymce -->
+<script src="{{ asset('backend/assets/vendors/tinymce/tinymce.min.js') }}"></script>
+<script src="{{ asset('backend/assets/js/tinymce.js') }}"></script>
+<!-- End tinymce -->
+
 </body>
 </html>
